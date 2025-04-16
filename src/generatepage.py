1c95f31ff401c3f965a15bfcce27f36eb9c04037
KasjanK <kowalczykkasjan@gmail.com>
2025-04-16 22:53:05 +0200

    refactor: set up the project for gitgub pages

--- src/generatepage.py
+++ src/generatepage.py
@@ -9,7 +9,7 @@ def extract_title(markdown):
         else:
             return block[2:].strip()
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     markdown_file = open(from_path, "r")
     read_markdown_file = markdown_file.read()
@@ -25,6 +25,9 @@ def generate_page(from_path, template_path, dest_path):
     page_title = extract_title(read_markdown_file)
     read_template_file = read_template_file.replace("{{ Title }}", page_title)
     read_template_file = read_template_file.replace("{{ Content }}", html_string)
+    read_template_file = read_template_file.replace('href="/', f'href="{basepath}')
+    read_template_file = read_template_file.replace('src="/', f'src="{basepath}')
+
 
     destination_dir_path = os.path.dirname(dest_path)
     if destination_dir_path != "":
@@ -32,7 +35,7 @@ def generate_page(from_path, template_path, dest_path):
     full_html_page = open(dest_path, "w")
     full_html_page.write(read_template_file)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     content_items = os.listdir(dir_path_content)
 
     for item in content_items:
@@ -40,6 +43,6 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         destination_item = os.path.join(dest_dir_path, item)
 
         if os.path.isdir(content_item):
-            generate_pages_recursive(content_item, template_path, destination_item)
+            generate_pages_recursive(content_item, template_path, destination_item, basepath)
         elif content_item.endswith(".md"):
-            generate_page(content_item, template_path, destination_item[:-2] + "html")
+            generate_page(content_item, template_path, destination_item[:-2] + "html", basepath)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
-import os
+import os, sys
 from generatepage import generate_pages_recursive
 from copystatic import copy_contents
 
+basepath = "/"
+if len(sys.argv) > 1:
+    basepath = sys.argv[1]
+
 def main():
-    copy_contents(os.path.abspath(os.path.expanduser("static")), os.path.abspath(os.path.expanduser("public")))
-    generate_pages_recursive("./content", "./template.html", "./public")
+    copy_contents(os.path.abspath(os.path.expanduser("static")), os.path.abspath(os.path.expanduser("docs")))
+    generate_pages_recursive("./content", "./template.html", "./docs", basepath)
 
 if __name__ =="__main__":
     main()
